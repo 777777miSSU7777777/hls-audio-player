624105b216f824aae49de24ac772a2f7ad3c2261
777777miSSU7777777 <o.fedorovich@yumasoft.com>
2020-09-09 13:39:39 +0300

    setup scss modules & move dev dependencies

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      HLS Audio Player
+    </div>
+  );
+}
+
+export default App;
