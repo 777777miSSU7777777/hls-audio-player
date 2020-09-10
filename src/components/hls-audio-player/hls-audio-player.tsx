3b49fe0322fdd8112ff4943b242261587c1e97db
777777miSSU7777777 <o.fedorovich@yumasoft.com>
2020-09-10 16:36:48 +0300

    add volume control

--- src/components/hls-audio-player/hls-audio-player.tsx
+++ src/components/hls-audio-player/hls-audio-player.tsx
@@ -3,6 +3,8 @@ import styles from './hls-audio-player.module.scss';
 import Hls from 'hls.js';
 import playButton from '../../assets/icons/play-button.svg';
 import pauseButton from '../../assets/icons/pause-button.svg';
+import volumeOnButton from '../../assets/icons/volume-on-button.svg';
+import volumeOffButton from '../../assets/icons/volume-off-button.svg';
 
 interface Props {
     hlsSource: string;
@@ -15,6 +17,8 @@ const HLSAudioPlayer = (props: Props) => {
     const [duration, setDuration] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [volume, setVolume] = useState<number>(1);
+    const [isMuted, setIsMuted] = useState<boolean>(false);
 
     useEffect(() => {
         if (hlsRef.current) {
@@ -67,6 +71,17 @@ const HLSAudioPlayer = (props: Props) => {
         }
     }
 
+    const onVolumeChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const newVolume: number = +e.currentTarget.value;
+        setVolume(newVolume);
+    }
+
+    useEffect(() => {
+        audioRef.current!.volume = volume;
+    }, [volume]);
+
+    const onMute = () => setIsMuted(!isMuted);
+
     return (
         <div className={styles.hlsAudioPlayer}>
             <audio 
@@ -74,6 +89,7 @@ const HLSAudioPlayer = (props: Props) => {
                 onTimeUpdate={onProgress}
                 onPlay={onPlay}
                 onPause={onPause}
+                muted={isMuted}
             />
             <div className={styles.leftControls}>
                 <div className={styles.playPauseButton} onClick={onPlayPauseClick}>
@@ -81,6 +97,9 @@ const HLSAudioPlayer = (props: Props) => {
                 </div>
             </div>
             <div className={styles.progressWrapper}>
+                <div className={styles.timeline} />
+                <div className={styles.progressLine} style={{ width: `${currentTime / duration * 100}%`}} />
+                <div className={styles.progressThumb} style={{ marginLeft: `calc(${currentTime / duration * 100 }% - 8px)`}} />
                 <input 
                     type='range' 
                     min='0'
@@ -92,9 +111,26 @@ const HLSAudioPlayer = (props: Props) => {
                     onMouseDown={onSeekStart}
                     onMouseUp={onSeekEnd}
                 />
-                <div className={styles.timeline} />
-                <div className={styles.progressLine} style={{ width: `${currentTime / duration * 100}%`}} />
-                <div className={styles.progressThumb} style={{ marginLeft: `calc(${currentTime / duration * 100 }% - 8px)`}} />
+            </div>
+            <div className={styles.volumeControls}>
+                <div className={styles.volumeWrapper}>
+                    <div className={styles.volumeLine} />
+                    <div className={styles.volumeLevelLine} style={{ width: `${volume * 100}%` }}/>
+                    <div className={styles.volumeThumb} style={{ marginLeft: `calc(${volume * 100 }% - 8px)`}} />
+                    <input
+                        type='range'
+                        min='0'
+                        max='1'
+                        step='0.01'
+                        className={styles.rangeInput}
+                        onInput={onVolumeChange}
+                        onChange={onVolumeChange}
+                    />
+                </div>
+
+                <div className={styles.volumeButton} onClick={onMute}>
+                    <img src={isMuted || volume === 0 ? volumeOffButton : volumeOnButton} />
+                </div>
             </div>
         </div>
     )
